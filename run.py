18efaeb91ca5532fcc276bfff18683b3cbb323ce
glitznerf <glitznerf@gmail.com>
2020-05-01 16:01:44 +0200

    Added ability to send text to latest conversation with randomization

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ sys.path.insert(0, '../')                                   # Join path for logi
 # Store username and password in a keys.py file in the parent folder's directory
 from keys import username, password
 
+
 class MessengerBot():
     def __init__(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install()) # Install Chrome driver if necessary
@@ -15,10 +16,12 @@ class MessengerBot():
     # Log into Facebook Messenger
     def login(self):
         # Open Messenger and wait for website to load
+        print("Opening up messenger...")
         self.driver.get('https://messenger.com')
         sleep(5)
 
         # Navigate login page and insert credentials using xpaths found by website inspector
+        print("Logging into messenger...")
         email = self.driver.find_element_by_xpath('//*[@id="email"]')
         email.click()
         email.send_keys(username)
@@ -30,6 +33,24 @@ class MessengerBot():
         # Log into Messenger
         login_button = self.driver.find_element_by_xpath('//*[@id="loginbutton"]')
         login_button.click()
+        print("Successfully logged into messenger!")
+
+    # Text in the latest open conversation
+    def text_latest(self, msg):
+        # Navigate message line on absolute path due to random class name initialization
+        msg_line = self.driver.find_element_by_xpath('/html/body/div/div/div/div[2]/span/div[2]/div[2]/div[2]/div[2]/div[3]/div/div/div[1]/div/div/div/div')
+        sleep(0.1)
+        msg_line.click()
+        # Insert each letter individually and randomize speed to stay undetected
+        for letter in msg:
+            msg_line.send_keys(letter)
+            sleep(random.randrange(1,5)/10)
+
+        # Send message with random delay
+        sleep(random.randrange(2,10)/10)
+        send_button = self.driver.find_element_by_xpath('/html/body/div/div/div/div[2]/span/div[2]/div[2]/div[2]/div[2]/a')
+        send_button.click()
+
 
 # Load up bot object and log into Messenger
 bot = MessengerBot()
